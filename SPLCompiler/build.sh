4355c0307a4e82225e89ec8a30a8cc1d18107915
YuChing Chiang <85006801+cotty0311@users.noreply.github.com>
2021-06-08 21:01:29 +0800

    加入g++版本
    g++ 加入 -std=c++14

--- SPLCompiler/build.sh
+++ SPLCompiler/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 flex -o "./src/Scanner.cc" "./src/Scanner.l"
 bison -o "./src/Parser.cc" -d "./src/Parser.yy" 
-g++ "./src/main.cc" "./src/Parser.cc" "./src/Scanner.cc" "./src/log.cc" "./src/ast.cc" "./src/table.cc" `llvm-config --cxxflags --ldflags --system-libs --libs core orcjit native` -frtti -o "./bin/main"
+g++ "./src/main.cc" "./src/Parser.cc" "./src/Scanner.cc" "./src/log.cc" "./src/ast.cc" "./src/table.cc" `llvm-config --cxxflags --ldflags --system-libs --libs core orcjit native` -frtti -o "./bin/main" -std=c++14
